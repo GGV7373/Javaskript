5ea167bbe0a820606326908072f244e022ce32b2
GGV7373 <GGVictor7373@outlook.com>
2025-08-27 12:09:29 +0200

    1-4 opp

--- JSintor/script.js
+++ JSintor/script.js
@@ -1,1 +1,14 @@
 alert('hello world from script.js');
+
+let navn = prompt('hva heter du?');
+document.write('hei ' + navn);
+
+let høyde = window.innerHeight;
+document.write(' høyden på vinduet er ' + høyde);
+
+let breddee = window.innerWidth;
+document.write(' bredden på vinduet er ' + breddee);
+
+let navn2 = prompt('Hva skal på siden?') 
+
+document.body.innerHTML += '<h2>' + navn2 + '</h2>';
