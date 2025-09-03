8d794f7d37bbe876dd054bf4340b21069612fa47
GGV7373 <GGVictor7373@outlook.com>
2025-09-03 12:45:59 +0200

    add 5,6,7,8
    addet task 5, 6, 7, 8 and folder oppgave_9

--- JSvariabler/script.js
+++ JSvariabler/script.js
@@ -12,8 +12,9 @@ else{
 let nanvn = prompt("Hva hetter du?");
 
 for(let i = 0; i < 11; i++) {
-    document.write(" "+ nanvn + " " + i)
+    document.write(nanvn + " ")
     i +=1; }
+document.write("<br>")
 
 // oppgave 3
 document.write("Svar er " + (15 % 2) + "<br>");
@@ -27,5 +28,41 @@ let age = prompt("Hvor gamme er du?");
 
 if (age >= 21) {
     let svar = document.getElementById('svar').innerHTML = 'Du kan drike ute på pub'; }
-else{
+else {
     let svar = document.getElementById('svar').innerHTML = 'Du kan ikke drike ute på pub'; }
+
+// oppgave 5
+let sum = 0
+for(let i = 0; i < 101; i++) {
+    sum = sum + i;
+}
+document.write(sum + "<br>");
+
+// oppgave 6
+let sum2 = 0
+for(let i = 0; i < 101; i++) {
+    if (i % 2 === 1){
+        sum2 = sum2 + i;
+    }
+}
+document.write(sum2 + "<br>");
+
+// oppgeve 7
+let sum3 = 0
+for(let i = 0; i < 101; i++) {
+    if (i % 3 === 0){
+        sum3 = sum3 + i;
+    }
+}
+document.write(sum3 + "<br>");
+
+// oppgave 8
+
+let sum4 = 0
+let legde = prompt("hvor lagt skal den telle?")
+for(let i = 0; i < legde; i++) {
+    if (i % 3 === 0){
+        sum4 = sum4 + i;
+    }
+}
+document.write(sum4 + "<br>");
