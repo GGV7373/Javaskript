c0d6803e3fd9bfe05a39f14f8893a960ca36cad2
GGV7373 <GGVictor7373@outlook.com>
2025-09-10 14:29:43 +0200

    add funksjoner and fixs JShendelser/opp_3

--- JShendelser/oppgeve_3/script.js
+++ JShendelser/oppgeve_3/script.js
@@ -1,6 +1,8 @@
 document.querySelectorAll(".knap").forEach(function(button) {
     button.addEventListener("click", function() {
-        if (Math.floor(Math.random() * 6) === 5)
+        const matte = Math.floor(Math.random() * 6);
+        console.log(matte)
+        if (matte === 5)
             alert("Du fant knappen");
         else
             alert("Feil knapp");
